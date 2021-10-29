65d99b965c931fbac870c5a7308ae7591d4b207d
Priyansh Singh <singhpriyansh2000@gmail.com>
2021-10-30 00:38:34 +0530

    A few UI changes & added the cart modal

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
     <Fragment>
       <header className={classes.header}>
         <h1>MealStore</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="food" />
